6bfd97eb0e736d32e2a0a1ebb9c63a49f7776a43
Manish Sharma <imac-1@iMac-1s-MacBook-Pro.local>
2019-05-27 17:37:33 +0530

    TDD 1

---
+++ UnitTestDemo/PlayData.swift
@@ -0,0 +1,19 @@
+//
+//  PlayData.swift
+//  UnitTestDemo
+//
+//  Created by Manish Sharma on 27/05/19.
+//  Copyright © 2019 Manish Sharma. All rights reserved.
+//
+
+import Foundation
+
+class PlayData {
+    var allwords = [String]()
+    
+    func myName() -> String {
+        let name = "Manish"
+        return name
+    }
+        
+}
--- UnitTestDemoTests/UnitTestDemoTests.swift
+++ UnitTestDemoTests/UnitTestDemoTests.swift
@@ -18,17 +18,39 @@ class UnitTestDemoTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testAllWordLoaded() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.allwords.count,0, "Allow elements must be zero")
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testMyName() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.myName(), "Manish", "Wrong name found")
     }
+    
+    func testLoop() {
+        // given
+        
+        // when
+        
+        // then
+        
+    }
+    
+    
+    
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
